06e0d19e4656de2a45ea53e74903c5337ae792ff
Elias Roussos <eliasisaiah@outlook.com>
2023-05-21 01:50:57 -0400

    modification to page

--- wordle-helper/src/routes/+page.svelte
+++ wordle-helper/src/routes/+page.svelte
@@ -1,3 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Hopefully there will be more content here soon 😊</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Elias Roussos</h1>
+<p>software developer</p>
+
+<p>Created with Svelte. Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
