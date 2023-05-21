be1803c6370b4dec44164474c60f2408845c6098
Elias Roussos <eliasisaiah@outlook.com>
2023-05-21 02:18:59 -0400

    added moved about me info into its own component

--- wordle-helper/src/routes/+page.svelte
+++ wordle-helper/src/routes/+page.svelte
@@ -1,9 +1,7 @@
-<h1>Elias Roussos</h1>
-<ul>
-    <li>ally 🏳‍🌈</li>
-    <li>software developer</li>
-    <li>cooking enthusiast</li>
-    <li>great pottery throw-down enjoyer</li>
-</ul>
+<script>
+    import AboutMe from "../components/AboutMe.svelte";
+</script>
+
+<AboutMe />
 
 <p>Created with Svelte. Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
