33c0c9deb2a286c82ca13cb31639819db7d783a8
Elias Roussos <eliasisaiah@outlook.com>
2023-05-21 02:18:39 -0400

    added AboutMe component

---
+++ wordle-helper/src/components/AboutMe.svelte
@@ -0,0 +1,8 @@
+<h1>Elias Roussos</h1>
+<h3>This is a separate Svelte component</h3>
+<ul>
+    <li>ally 🏳‍🌈</li>
+    <li>software developer</li>
+    <li>cooking enthusiast</li>
+    <li>great pottery throw-down enjoyer</li>
+</ul>
