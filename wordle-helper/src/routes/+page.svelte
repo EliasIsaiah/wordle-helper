6249256bbb8e754e359512beb98e0548fb0635b9
Elias Roussos <eliasisaiah@outlook.com>
2023-05-21 02:08:04 -0400

    update about me

--- wordle-helper/src/routes/+page.svelte
+++ wordle-helper/src/routes/+page.svelte
@@ -3,6 +3,7 @@
     <li>ally 🏳‍🌈</li>
     <li>software developer</li>
     <li>cooking enthusiast</li>
+    <li>great pottery throw-down enjoyer</li>
 </ul>
 
 <p>Created with Svelte. Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
