eb74601fd6a8474deebbe0336616aa0e18573b8f
Elias Roussos <eliasisaiah@outlook.com>
2023-07-16 23:28:30 -0400

    implemented rainbow cursor

--- wordle-helper/src/routes/+page.svelte
+++ wordle-helper/src/routes/+page.svelte
@@ -1,24 +1,33 @@
 <script>
-    import AboutMe from "../components/AboutMe.svelte";
+	import AboutMe from '../components/AboutMe.svelte';
+	import { rainbowCursor } from 'cursor-effects';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+        let container = document.getElementsByClassName("container")[0];
+		new rainbowCursor({ element: container });
+	});
 </script>
 
 <div class="container">
-<AboutMe />
+	<AboutMe />
 
-<p>Created with Svelte. Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<p>
+		Created with Svelte. Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+	</p>
 </div>
 
 <style>
-    .container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 90vh;
-    }
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 90vh;
+	}
 
-    .container p {
-        margin: 2em;
-        padding: 2em;
-        background-color: oldlace;
-    }
+	.container p {
+		margin: 2em;
+		padding: 2em;
+		background-color: oldlace;
+	}
 </style>
